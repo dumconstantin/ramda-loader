eabba98e176e90578e9962222143c54ddaff3f94
dumconstantin <dum.constantin@gmail.com>
2016-05-05 18:11:36 +0300

    Change error text from [ramda-global-loader] to [ramda-loader]

--- src/redeclareError.js
+++ src/redeclareError.js
@@ -1,7 +1,7 @@
 const redeclareError = (ctx, rowLoc, charLoc, name) => {
   return new Error(`
 
-  [ramda-global-loader]
+  [ramda-loader]
   Error: "${name}" is redeclared in ${ctx.file}:${rowLoc}:${charLoc}
   RamdaJs functions shouldn't be redeclared if you want to use RamdaJs without the R. namespace.
 
